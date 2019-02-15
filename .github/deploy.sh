ce997ebef704b5edec4b8e8c39d6658827fea109
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 17:21:55 -0500

    test deploy

---
+++ .github/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+whoami | groups
+sudo chmod -R 777 /github/home/
+pip install --user --no-cache-dir -r requirements.txt          
+~/.local/bin/pyinstaller -F hello_world.py
+export TAG=${GITHUB_SHA}
+export IMAGE_NAME='python-cicd-workshop'
+docker build -t $DOCKER_LOGIN/$IMAGE_NAME -t $DOCKER_LOGIN/$IMAGE_NAME:$TAG .
+echo $DOCKER_PWD | docker login -u $DOCKER_LOGIN --password-stdin
+docker push $DOCKER_LOGIN/$IMAGE_NAME
