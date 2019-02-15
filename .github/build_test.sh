e516f4a8fdca9ec8ddec9ad4adc0d4b59837afdb
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 18:09:00 -0500

    Fixed the file access permissions

--- .github/build_test.sh
+++ .github/build_test.sh
@@ -3,7 +3,7 @@ set -e
 set -o pipefail
 
 whoami | groups
-sudo chmod -R 777 /github/home/
+sudo chmod -R 777 /github/
 pip install --user --upgrade pip==18.0
 pip install --user --no-cache-dir -r requirements.txt
 python test_hello_world.py
