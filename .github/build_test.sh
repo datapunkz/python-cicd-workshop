85eb281a9e984758b99d5252428c6852a774b173
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 18:02:54 -0500

    checkin new files

--- .github/build_test.sh
+++ .github/build_test.sh
@@ -4,5 +4,6 @@ set -o pipefail
 
 whoami | groups
 sudo chmod -R 777 /github/home/
+pip install --user --upgrade pip==18.0
 pip install --user --no-cache-dir -r requirements.txt
 python test_hello_world.py
