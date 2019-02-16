50636d80b9f7e3cf5d699cbfce01f2facf249ffe
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 21:21:12 -0500

    test secrets again

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -6,12 +6,12 @@ set -o pipefail
 sudo chmod -R 777 /github/
 # pip install --user --upgrade pip==18.0
 # pip install --user --no-cache-dir -r requirements.txt        
-echo ${DOCKER_LOGIN}
-echo ${DOCKER_PWD}  
+# echo ${DOCKER_LOGIN}
+# echo ${DOCKER_PWD}  
 ~/.local/bin/pyinstaller -F hello_world.py
 export TAG=${GITHUB_SHA}
 export IMAGE_NAME='python-cicd-workshop'
 export DOCKER_IMAGE_NAME=${DOCKER_LOGIN}/${IMAGE_NAME}
-docker build -t ${DOCKER_IMAGE_NAME} -t ${DOCKER_IMAGE_NAME}:${TAG} .
-echo ${DOCKER_PWD} | docker login -u ${DOCKER_LOGIN} --password-stdin
+# docker build -t ${DOCKER_IMAGE_NAME} -t ${DOCKER_IMAGE_NAME}:${TAG} .
+# echo ${DOCKER_PWD} | docker login -u ${DOCKER_LOGIN} --password-stdin
 docker push ${DOCKER_IMAGE_NAME}
--- .github/main.workflow
+++ .github/main.workflow
@@ -8,9 +8,15 @@ action "build_test" {
   runs = "./.github/build_test.sh"
 }
 
+action "Docker Login" {
+  uses = "actions/docker/login@master"
+  secrets = ["DOCKER_LOGIN", "DOCKER_PWD"]
+  needs = "build_test"
+}
+
 action "deploy" {
   uses = "docker://circleci/python:2.7.15"
   runs = "./.github/deploy.sh"
-  needs = "build_test"
+  needs = "Docker Login"
   secrets = ["DOCKER_LOGIN", "DOCKER_PWD"]
 }
