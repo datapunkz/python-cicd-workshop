99d4282143c727b56088248a93b93cb41d70fbd1
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 20:31:17 -0500

    test deploy

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -8,6 +8,7 @@ pip install --user --no-cache-dir -r requirements.txt
 ~/.local/bin/pyinstaller -F hello_world.py
 export TAG=${GITHUB_SHA}
 export IMAGE_NAME='python-cicd-workshop'
-docker build -t $DOCKER_LOGIN/$IMAGE_NAME -t $DOCKER_LOGIN/$IMAGE_NAME:$TAG .
-echo $DOCKER_PWD | docker login -u $DOCKER_LOGIN --password-stdin
-docker push $DOCKER_LOGIN/$IMAGE_NAME
+export DOCKER_IMAGE_NAME=${DOCKER_LOGIN}/${IMAGE_NAME}
+docker build -t ${DOCKER_IMAGE_NAME} -t ${DOCKER_IMAGE_NAME}:${TAG} .
+echo ${DOCKER_PWD} | docker login -u ${DOCKER_LOGIN} --password-stdin
+docker push ${DOCKER_IMAGE_NAME}
