a2388af6abab3ef9b2183dc1072c816617ba9de8
Angel Rivera <2404388+ariv3ra@users.noreply.github.com>
2019-02-15 20:56:22 -0500

    Added secrets to actions

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -8,7 +8,6 @@ pip install --user --no-cache-dir -r requirements.txt
 ~/.local/bin/pyinstaller -F hello_world.py
 export TAG=${GITHUB_SHA}
 export IMAGE_NAME='python-cicd-workshop'
-export DOCKER_LOGIN='ariv3ra'
 export DOCKER_IMAGE_NAME=${DOCKER_LOGIN}/${IMAGE_NAME}
 docker build -t ${DOCKER_IMAGE_NAME} -t ${DOCKER_IMAGE_NAME}:${TAG} .
 echo ${DOCKER_PWD} | docker login -u ${DOCKER_LOGIN} --password-stdin
--- .github/main.workflow
+++ .github/main.workflow
@@ -12,4 +12,5 @@ action "deploy" {
   uses = "docker://circleci/python:2.7.15"
   runs = "./.github/deploy.sh"
   needs = "build_test"
+  secrets = ["DOCKER_LOGIN", "DOCKER_PWD"]
 }
